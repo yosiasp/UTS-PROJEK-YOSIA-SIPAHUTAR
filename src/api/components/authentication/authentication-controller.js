61b1fb7a75fa6196e7786be89889724f1ebd5ad2
yosiasp <yosia.535230117@stu.untar.ac.id>
2024-05-01 19:44:04 +0700

    Perubahan pertama pada atuhentication-controller

--- src/api/components/authentication/authentication-controller.js
+++ src/api/components/authentication/authentication-controller.js
@@ -1,5 +1,6 @@
 const { errorResponder, errorTypes } = require('../../../core/errors');
 const authenticationServices = require('./authentication-service');
+const loginAttempts = {}; // Object to track login attempts for each email
 
 /**
  * Handle login request
@@ -12,20 +13,57 @@ async function login(request, response, next) {
   const { email, password } = request.body;
 
   try {
-    // Check login credentials
+    // Check if there are previous failed attempts for this email
+    if (!loginAttempts[email]) {
+      loginAttempts[email] = {
+        count: 0,
+        lastAttempt: null,
+      };
+    }
+
+    const { count, lastAttempt } = loginAttempts[email];
+
+    if (lastAttempt && Date.now() - lastAttempt < 30 * 60 * 1000) {
+      if (count >= 5) {
+        const minutesLeft = Math.ceil(
+          (lastAttempt + 30 * 60 * 1000 - Date.now()) / (60 * 1000)
+        );
+        throw errorResponder(
+          errorTypes.FORBIDDEN,
+          `Too many failed login attempts. Please try again in ${minutesLeft} minutes.`
+        );
+      }
+    } else {
+      loginAttempts[email].count = 0;
+    }
+
     const loginSuccess = await authenticationServices.checkLoginCredentials(
       email,
       password
     );
 
     if (!loginSuccess) {
+      loginAttempts[email].count++;
+      loginAttempts[email].lastAttempt = Date.now();
+      const currentDate = new Date().toLocaleDateString();
+      const loginTime = new Date().toLocaleTimeString();
       throw errorResponder(
         errorTypes.INVALID_CREDENTIALS,
-        'Wrong email or password'
+        `Wrong email or password for user ${email}. Total failed attempts: ${loginAttempts[email].count}. Date: ${currentDate}. Login Time: ${loginTime}`
       );
     }
 
-    return response.status(200).json(loginSuccess);
+    loginAttempts[email].count = 0;
+    loginAttempts[email].lastAttempt = null;
+
+    // Login successful, add login time to the response
+    const loginTime = new Date().toLocaleTimeString();
+    const responseData = {
+      ...loginSuccess,
+      loginTime: loginTime,
+    };
+
+    return response.status(200).json(responseData);
   } catch (error) {
     return next(error);
   }
