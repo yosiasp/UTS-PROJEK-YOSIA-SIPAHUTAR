ce8a16b1a86d44d56fdd94ebe62bff63bfbe6b6a
yosiasp <yosia.535230117@stu.untar.ac.id>
2024-05-06 01:06:39 +0700

    MENAMBAHKAN SKEMA PADA MARKETPLACE

---
+++ src/models/marketplace-shema.js
@@ -0,0 +1,10 @@
+const marketplaceSchema = {
+  model: String,
+  brand: String,
+  category: String,
+  price: Number,
+  stock: Number,
+  quantity: Number,
+}
+
+module.exports = marketplaceSchema;
