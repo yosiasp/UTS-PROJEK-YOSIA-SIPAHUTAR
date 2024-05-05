70f31f0a19ed3ec1a0856ba54d747f0718ab367f
yosiasp <yosia.535230117@stu.untar.ac.id>
2024-05-06 01:12:09 +0700

    MEMASUKAN ROUTE MARKETPLACE KE INDEX

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const authentication = require('./components/authentication/authentication-route');
 const users = require('./components/users/users-route');
+const marketplace = require('./components/marketplace/marketplace-route'); // add this line
 
 module.exports = () => {
   const app = express.Router();
 
   authentication(app);
   users(app);
+  marketplace(app); 
 
   return app;
 };
